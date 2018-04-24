717eed724a3a218805d429e9d32916e3e744d1bd
agnesdonat <agi.donat@gmail.com>
2018-04-24 14:34:26 +0100

    Account stores latest debit information

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,20 @@
 class Account
-  attr_reader :balance, :credit
+  attr_reader :balance, :credit, :debit
 
   def initialize
     @balance = 0
     @credit = 0
+    @debit = 0
   end
 
   def make_a_deposit(money)
-    @balance += money
     @credit = money
+    @balance += @credit
   end
 
   def make_a_withdrawal(money)
     raise "Insufficient funds" if money > @balance
+    @debit = money
     @balance -= money
   end
 end
